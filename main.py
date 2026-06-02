0a9678449d6a860907355b8cdd800928eabb76e6
Akash Nagar <148152078+techakash32@users.noreply.github.com>
2026-06-02 16:57:47 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.cnnClassifier import logger 
 import logging 
-print(logging.info("Welcome to PYTHON ADVANCE BATCH 1 logs"))  
+# print(logging.info("Welcome to Python Advance Batch-1 logs"))  
+
+
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
